841c10feee57a0b4a59bbcda98639d82362a5a0c
ivo- <ivailo.nikolaev.hristov@gmail.com>
2015-04-25 20:15:31 +0300

    Prevent host stealing from older connections

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ var config = {
 
 var PeerServer = require('peer').PeerServer;
 var log        = console.log.bind(console);
-var database   = { domains: {}, hosts: {} };
+var database   = { domains: {}, hosts: {}, recent: {} };
 
 // =============================================================================
 // Server
@@ -16,8 +16,18 @@ var database   = { domains: {}, hosts: {} };
 var server = PeerServer(config);
 console.log("Server started on port: " + config.port);
 
-server.on('connection', function (id, domain, socket) {
+server.on('connection', function handleConnection (id, domain, socket) {
     if (!database.domains.hasOwnProperty(domain)) {
+        // New connections cannot be pronounced as hosts until domain is removed
+        // from recent list.
+        if (database.recent[domain]) {
+            log("[" + id + "] Wait for recent domain: " + domain);
+            setTimeout(function() {
+                handleConnection(id, domain, socket);
+            }, 100);
+            return;
+        }
+
         database.domains[domain] = socket;
         database.hosts[id] = domain;
 
@@ -38,18 +48,27 @@ server.on('disconnect', function (id) {
         delete database.hosts[id];
         delete database.domains[domain];
 
+        // Wait 10s after host disconnects before enabling pronouncing new
+        // connections as hosts. Give time to older connections to claim their
+        // holy right to become hosts.
+        database.recent[domain] = true;
+        setTimeout(function() {
+            delete database.recent[domain];
+        }, 10000);
+
         log("[" + id + "] Disconnected HOST for domain: " + domain);
     } else {
         log("[" + id + "] Disconnected.");
     }
 });
 
-// TODO: Wait 2s after host disconnects and then accept new hosts different
-// than claim.
 server.on('host-claim', function(id, domain, socket) {
     if (database.domains[domain]) {
         log("[" + id + "] Trying to claim an owned domain: " + domain);
     } else {
+        // We have host claim from older connection for domain.
+        delete database.recent[domain];
+
         database.domains[domain] = socket;
         database.hosts[id] = domain;
 
